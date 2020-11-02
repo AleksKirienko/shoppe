714b6c722cb44c2368c459e3d48ef541f5663508
AleksKirienko <aleks.Kirienko@bk.ru>
2020-11-02 18:53:11 +0700

    Backlight with setInterval

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 let elements = document.querySelectorAll("section.content > div.card-md, div.top__card-md, div.left__card-lg, div.left__card-sm");
 
-let color = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase() + " 3px solid";
+let color;
 
-for (let elem of elements) {
-    if (Math.round(Math.random()) === 1)
-        elem.style.border = color;
-    console.log(elements.innerHTML);
+function checkRand(c) {
+    for (let elem of elements) {
+        if (Math.round(Math.random()) === 1)
+            elem.style.border = c;
+        console.log(elements.innerHTML);
+    }
 }
 
-let timeoutId = setTimeout(function () {
-    window.location.reload(1);
+let timeoutId = setInterval(function () {
+    color = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase() + " 3px solid";
+    checkRand(color);
+    checkRand("white");
 }, 2000);
 
-// clearTimeout(timeoutId);
+// clearInterval(intervalId);
