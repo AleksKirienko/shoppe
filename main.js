c301da1a3f74c7c5b349325eb583b1f5fc6baa0b
AleksKirienko <aleks.Kirienko@bk.ru>
2020-11-02 17:29:59 +0700

    Update backlight

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 let elements = document.querySelectorAll("section.content > div.card-md, div.top__card-md, div.left__card-lg, div.left__card-sm");
 
-let color = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
-
+let color = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase() + " 3px solid";
 
 for (let elem of elements) {
     if (Math.round(Math.random()) === 1)
-        elem.style.background = color;
+        elem.style.border = color;
     console.log(elements.innerHTML);
 }
 
-setTimeout(function () {
+let timeoutId = setTimeout(function () {
     window.location.reload(1);
 }, 2000);
+
+// clearTimeout(timeoutId);
